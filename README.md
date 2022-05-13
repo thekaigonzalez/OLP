85ac2373f8157769085cb85e5c43f5492c737ae5
Kai D. Gonzalez <gkai70264@gmail.com>
2022-05-13 13:53:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OLP
+
+A quick, and fast password generator algorithm.
+
+Written in python, the program takes around `38` milliseconds on an Intel i5 CPU (generating a simple 15 char string), 
+and around `37` for an 1000 character string, `89` on a 10,000 character string.
---
+++ main.py
@@ -0,0 +1,58 @@
+import hashlib
+import os
+import string
+import sys
+
+def generate_password(
+    len): 
+    """
+    Generates a password
+
+    When this algorithm is run, it will use a range with 1-9, [a-Z], and extra symbols, and create
+    an array based on that string, then it will check the length, and continue adding random characters from the
+    character array.
+    
+    """
+    import random
+
+    length = 0
+    rand = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
+    rage = [ch for ch in rand]
+    gen = ""
+
+
+    while (length < len):
+        gen += random.choice(rage)
+        length += 1
+
+    return gen
+
+def save_password_hash(string):
+    """
+    Saves the password to the user's home directory in .olp-passwords 
+    as a hash.
+    """
+    open(os.path.expanduser("~/.olp-passwords"), "a").write(": password : " + string[0:2] + "... : " + str(hashlib.sha1(string.encode("utf-8")).hexdigest() + "\n"))
+
+# variables controlled by args
+save = False
+length = 15
+
+# parse args
+for i in sys.argv:
+    if (i == '--save' or i == '-s'):
+        print("saving password")
+        save = True
+    elif i.startswith("-l"):
+        length = int(i[2:])
+
+    
+passwd = generate_password(length)
+
+print(passwd)
+
+if (save):
+    save_password_hash(passwd)
+
+
+    
